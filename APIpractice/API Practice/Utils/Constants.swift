8b08908fb7e8e395181e789a4230710f87c3c85e
hsj._.06 <gtw030488@gmail.com>
2022-06-23 12:04:52 +0900

    updateAll

--- APIpractice/API Practice/Utils/Constants.swift
+++ APIpractice/API Practice/Utils/Constants.swift
@@ -11,3 +11,16 @@ enum SEGUE_ID {
     static let USER_LIST_VC = "GoToUserListVC"
     static let PHOTO_COLLECTION_VC = "GoToPhotoCollectionVC"
 }
+
+enum API {
+    
+    static let BASE_URL: String = "https://api.unsplash.com/"
+    
+    static let CLIENT_ID: String = "bsHmLmr6gp_JObbgbIWlVzc3J34O6YZVUZ3B3BvrygU"
+}
+
+enum NOTIFICATION {
+    enum API {
+        static let AUTH_FAIL = "authentication_fail"
+    }
+}
