3eede53042fe41c477bb304ef2c4731963615883
hsj._.06 <gtw030488@gmail.com>
2022-10-22 02:28:47 +0900

    🙏 :: VM 수정

--- team-comit/team-comit/Sources/Scene/ExchangeRateView/ViewModel/ExchangeRateViewModel.swift
+++ team-comit/team-comit/Sources/Scene/ExchangeRateView/ViewModel/ExchangeRateViewModel.swift
@@ -3,14 +3,18 @@ import Alamofire
 
 class ExchangeRateViewModel: ObservableObject {
     @Published var sendingCountry: String = ""
-    @Published var getingCountry: String = ""
-    @Published var exchangeRate: String = ""
+    @Published var gettingCountry: String = ""
+    @Published var exchangeRate: String = "0.00"
+    @Published var amount: String = "1"
     
     func calculate() {
+        print("calculating...")
+        print("gettingCountry :", gettingCountry)
+        print("sendingCountry :", sendingCountry)
         let url = "https://api.apilayer.com/currency_data/live"
         let param = [
             "source": sendingCountry,
-            "currencies": getingCountry
+            "currencies": gettingCountry
         ]
         let header: HTTPHeaders = [
             "apikey": "o4SXQyHc27lBbT4Ho71xOj63hlYxjfKT"
@@ -27,20 +31,25 @@ class ExchangeRateViewModel: ObservableObject {
             case .success(_):
                 switch result.response!.statusCode {
                 case 200 ..< 300:
-                    do {
-                        let model = try JSONDecoder().decode(ExchangeRateModel.self, from: result.data!)
-                        self.exchangeRate = String(model.quotes.exchangedRate)
-                    } catch {
-                        print("NON.DATA")
+                    DispatchQueue.main.async {
+                        do {
+                            let model = try JSONDecoder().decode(ExchangeRateModel.self, from: result.data!)
+                            self.exchangeRate = String(model.quotes[self.sendingCountry + self.gettingCountry]!)
+                            print("exchangeRate:", self.exchangeRate)
+                            print("amount:", self.amount)
+                            self.exchangeRate = String((Double(self.exchangeRate) ?? 0) * (Double(self.amount) ?? 0))
+                            print("송금 금액:", self.exchangeRate)
+                        } catch {
+                            print("NON.DATA")
+                        }
                     }
                 default:
-                    print("user's wrong ⚠️")
+                    print("user or server's wrong ⚠️")
                 }
             case .failure(_):
                 print("Failure..🚫")
             }
         }
     }
-    
 }
 
