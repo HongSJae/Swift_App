7daf97dbaec0b0ec409954179ab820d7be607d00
hsj._.06 <gtw030488@gmail.com>
2022-10-22 02:31:45 +0900

    🙏 :: 송금 / 수취 국가 입력

---
+++ team-comit/team-comit/Sources/Scene/ExchangeRateView/Text/DealText.swift
@@ -0,0 +1,37 @@
+//
+//  DealText.swift
+//  team-comit
+//
+//  Created by 홍승재 on 2022/10/21.
+//
+
+import SwiftUI
+
+struct DealText: View {
+    @Environment(\.colorScheme) var scheme
+    @Binding var pickerValue: Int
+    let titleText: String
+    let countrys: [String]
+    
+    var body: some View {
+        HStack {
+            Text(titleText)
+                .foregroundColor(Theme.fontColor(forScheme: scheme))
+                .font(.system(size: 18))
+                .fontWeight(.regular)
+            Spacer()
+            SelectCountryPicker(selected: $pickerValue, countrys: countrys)
+        }
+    }
+}
+
+struct DealText_Previews: PreviewProvider {
+    static var previews: some View {
+        DealText(pickerValue: .constant(0), titleText: "송금국가", countrys: [
+            "한국(KRW)",
+            "미국(USD)",
+            "일본(JPY)",
+            "필리핀(PHP)",
+        ])
+    }
+}
