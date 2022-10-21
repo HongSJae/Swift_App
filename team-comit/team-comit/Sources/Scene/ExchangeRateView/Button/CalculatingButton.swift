cdcc9e4b9d3e4531a7dea18751598b9fc54e3eb4
hsj._.06 <gtw030488@gmail.com>
2022-10-22 02:32:14 +0900

    🙏 :: 환율 계산 버튼

--- team-comit/team-comit/Sources/Scene/ExchangeRateView/Button/CalculatingButton.swift
+++ team-comit/team-comit/Sources/Scene/ExchangeRateView/Button/CalculatingButton.swift
@@ -9,25 +9,48 @@ import SwiftUI
 
 struct CalculatingButton: View {
     @Environment(\.colorScheme) var scheme
+    @StateObject var VM = ExchangeRateViewModel()
+    @State private var tag: Int?
+    @Binding var showingAlert: Bool
+    let amount: String
+    let unit_1: String
+    let unit_2: String
     var body: some View {
-        NavigationLink(destination: ChangedExchangeRateView()) {
-            Text("환율 계산")
-                .foregroundColor(Theme.fontColor(forScheme: scheme))
-                .font(.system(size: 18))
-                .fontWeight(.regular)
-                .frame(width: 114, height: 34)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(
-                            Theme.fontColor(forScheme: scheme),
-                            lineWidth: 1)
-                )
+        ZStack {
+            NavigationLink(destination: ChangedExchangeRateView(resultValue: $VM.exchangeRate, unit: unit_1), tag: 1, selection: $tag ) { EmptyView() }
+            Button {
+                if Double(amount) ?? 0 < 0
+                    || Double(amount) ?? 0 > 10000
+                    || Double(amount) == nil
+                {
+                    showingAlert = true
+                } else {
+                    tag = 1
+                    VM.amount = amount
+                    VM.gettingCountry = unit_1
+                    VM.sendingCountry = unit_2
+                    VM.calculate()
+                }
+            } label: {
+                Text("환율 계산")
+                    .foregroundColor(Theme.fontColor(forScheme: scheme))
+                    .font(.system(size: 18))
+                    .fontWeight(.regular)
+                    .frame(width: 114, height: 34)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(
+                                Theme.fontColor(forScheme: scheme),
+                                lineWidth: 1)
+                    )
+            }
         }
     }
 }
 
+
 struct CalculatingButton_Previews: PreviewProvider {
     static var previews: some View {
-        CalculatingButton()
+        CalculatingButton(showingAlert: .constant(false), amount: "1", unit_1: "USD", unit_2: "KRW")
     }
 }
