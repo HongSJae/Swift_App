d391a31d1f01130ac2c1fba4b6ae5ace160bc7d3
hsj._.06 <gtw030488@gmail.com>
2022-10-22 02:34:53 +0900

    🙏 :: 모델 수정

--- team-comit/team-comit/Model/ExchangeRateModel.swift
+++ team-comit/team-comit/Model/ExchangeRateModel.swift
@@ -2,17 +2,7 @@ import Foundation
 
 // MARK: - ExchangeRateModel
 struct ExchangeRateModel: Codable {
-    let success: Bool
-    let timestamp: Int
-    let source: String
-    let quotes: Quotes
+    let quotes: [String : Double]
 }
 
-// MARK: - Quotes
-struct Quotes: Codable {
-    let exchangedRate: Double
 
-    enum CodingKeys: String, CodingKey {
-        case exchangedRate = "USDKRW"
-    }
-}
