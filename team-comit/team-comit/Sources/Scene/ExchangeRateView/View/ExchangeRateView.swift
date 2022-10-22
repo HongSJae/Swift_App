e8c15f8de777810eef118b581b172690238186c4
hsj._.06 <gtw030488@gmail.com>
2022-10-22 16:47:59 +0900

    🙏 :: 기능 컨퍼넌트로 옮기기

--- team-comit/team-comit/Sources/Scene/ExchangeRateView/View/ExchangeRateView.swift
+++ team-comit/team-comit/Sources/Scene/ExchangeRateView/View/ExchangeRateView.swift
@@ -6,7 +6,6 @@ struct ExchangeRateView: View {
     @State private var reception: Int = 0
     @State private var amount: String = ""
     @State private var nowDate: String = ""
-    @State private var showingAlert: Bool = false
     let countrys = [
         "한국(KRW)",
         "미국(USD)",
@@ -28,34 +27,28 @@ struct ExchangeRateView: View {
 
         return "\(result) \(unit_1) / \(unit_2)"
     }
-    func exportDate() {
-        let formatter_year = DateFormatter()
-        formatter_year.dateFormat = "yyyy-MM-dd / HH:mm:ss"
-        let current_year_string = formatter_year.string(from: Date())
-        nowDate = current_year_string
-    }
     var body: some View {
         NavigationView {
             VStack(spacing: 16) {
                 TitleText()
                 DealText(pickerValue: $remittance,
-                         titleText: "송금국가", countrys: countrys)
+                         titleText: "송금국가",
+                         countrys: countrys)
                 .onChange(of: remittance) { newValue in
                     DispatchQueue.main.async {
                         exchangeRateViewModel.sendingCountry = units[newValue]
                         exchangeRateViewModel.gettingCountry = units[reception]
                         exchangeRateViewModel.calculate()
-                        exportDate()
                     }
                 }
                 DealText(pickerValue: $reception,
-                         titleText: "수취국가", countrys: countrys)
+                         titleText: "수취국가",
+                         countrys: countrys)
                 .onChange(of: reception) { newValue in
                     DispatchQueue.main.async {
                         exchangeRateViewModel.sendingCountry = units[remittance]
                         exchangeRateViewModel.gettingCountry = units[newValue]
                         exchangeRateViewModel.calculate()
-                        exportDate()
                     }
                 }
                 InformationText(titleText: "환율",
@@ -68,13 +61,9 @@ struct ExchangeRateView: View {
                 RemittanceAmountText(amount: $amount,
                                      unit: units[remittance])
                 Spacer()
-                CalculatingButton(showingAlert: $showingAlert,
-                                  amount: amount,
+                CalculatingButton(nowDate: $nowDate, amount: amount,
                                   unit_1: units[reception],
                                   unit_2: units[remittance])
-                .alert(isPresented: $showingAlert) {
-                            Alert(title: Text("송금액이 바르지 않습니다."), message: Text("송금액을 확인하고 다시 시도해주세요.\n0 < x < 10,000"), dismissButton: .default(Text("확인")))
-                        }
                 Spacer()
                 Spacer()
             }
@@ -84,12 +73,12 @@ struct ExchangeRateView: View {
             .padding(.horizontal, 24)
             .navigationBarHidden(true)
         }
-//        .onAppear() {
-//            exchangeRateViewModel.sendingCountry = units[remittance]
-//            exchangeRateViewModel.gettingCountry = units[reception]
-//            exchangeRateViewModel.calculate()
-//            exportDate()
-//        }
+        .onAppear() {
+            exchangeRateViewModel.sendingCountry = units[remittance]
+            exchangeRateViewModel.gettingCountry = units[reception]
+            exchangeRateViewModel.calculate()
+            nowDate = UserDefaults.standard.string(forKey: "date") ?? "전에 조회한 시간이 없어요"
+        }
     }
 }
 
