ea61ccef9dcde41b7a7bc6331c41ccd150c7a7f7
hsj._.06 <gtw030488@gmail.com>
2022-11-15 17:06:46 +0900

    Socket

---
+++ SocketTest/SocketTest/ViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+import SocketIO
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var connect: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    @IBAction func connecting(_ sender: UIButton) {
+        
+    }
+    
+
+}
