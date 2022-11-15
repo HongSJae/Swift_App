8fbe8c6a7f9a62ba444b052f9ca0558269afaf58
hsj._.06 <gtw030488@gmail.com>
2022-11-15 17:07:03 +0900

    Socket

---
+++ SocketTest/SocketTest/ChatTVC.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class ChatTVC: UITableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
