909a418f4c24c8fa25af62b1eacc37daa1670f75
hsj._.06 <gtw030488@gmail.com>
2022-12-02 17:36:07 +0900

    PasswordTextfield

--- DMSports/DMSports/Sources/Components/Auth/PasswordTextfield.swift
+++ DMSports/DMSports/Sources/Components/Auth/PasswordTextfield.swift
@@ -4,9 +4,6 @@ struct PasswordTextfield: View {
     @Binding var text: String
     @Binding var isSee: Bool
     let placeHolder: String
-    private func formEmail() -> Bool {
-        return text.contains("@dsm.hs.kr")
-    }
     private func eye() -> some View {
         return Button {
             self.isSee.toggle()
@@ -24,24 +21,30 @@ struct PasswordTextfield: View {
         }
 
     }
-    private func check() -> some View {
-        switch text.count >= 8 || text.count <= 20 {
-        case true:
-            return Image("Format OK")
-                .resizable()
-                .frame(width: 25, height: 20)
-        case false:
-            return Image("Format No")
-                .resizable()
-                .frame(width: 25, height: 20)
-        }
+    private func passwordTest() -> Bool {
+        return self.text.count >= 8 && self.text.count <= 20
     }
     var body: some View {
         HStack {
-            SecureField(placeHolder, text: $text)
-                .foregroundColor(.black)
-                .font(.custom("Inter-Regular", size: 16))
-            check()
+            switch isSee {
+            case true:
+                TextField(placeHolder, text: $text)
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+                    .foregroundColor(.black)
+                    .font(.custom("Inter-Regular", size: 16))
+            case false:
+                SecureField(placeHolder, text: $text)
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+                    .foregroundColor(.black)
+                    .font(.custom("Inter-Regular", size: 16))
+            }
+            if passwordTest() {
+                Image("Format OK")
+                    .resizable()
+                    .frame(width: 25, height: 20)
+            }
             eye()
         }
         .padding(.horizontal, 16)
@@ -50,15 +53,15 @@ struct PasswordTextfield: View {
         .overlay(
             RoundedRectangle(cornerRadius: 20)
                 .stroke(style: .init(lineWidth: 1))
-                .foregroundColor(formEmail() ? .primary2 : .hint)
+                .foregroundColor(passwordTest() ? .primary2 : .hint)
         )
     }
 }
 
 struct PasswordTextfield_Previews: PreviewProvider {
     static var previews: some View {
-        PasswordTextfield(text: .constant(""),
-                          isSee: .constant(true),
+        PasswordTextfield(text: .constant("afsdfasdf"),
+                          isSee: .constant(false),
                            placeHolder: "비밀번호")
     }
 }
