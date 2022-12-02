04d02921858178e78e5ce2becb31a5d8d5652b02
hsj._.06 <gtw030488@gmail.com>
2022-12-02 17:35:39 +0900

    EmailTextfield

--- DMSports/DMSports/Sources/Components/Auth/EmailTextfield.swift
+++ DMSports/DMSports/Sources/Components/Auth/EmailTextfield.swift
@@ -2,28 +2,22 @@ import SwiftUI
 
 struct EmailTextfield: View {
     @Binding var text: String
+    private func emailTest() -> Bool {
+        return self.text.contains("@dsm.hs.kr")
+    }
     let placeHolder: String
-    private func formEmail() -> Bool {
-        return text.contains("@dsm.hs.kr")
-    }
-    private func check() -> some View {
-        switch text.count >= 8 || text.count <= 20 {
-        case true:
-            return Image("Format OK")
-                .resizable()
-                .frame(width: 25, height: 20)
-        case false:
-            return Image("Format No")
-                .resizable()
-                .frame(width: 25, height: 20)
-        }
-    }
     var body: some View {
         HStack {
             TextField(placeHolder, text: $text)
+                .autocapitalization(.none)
+                .disableAutocorrection(true)
                 .foregroundColor(.black)
                 .font(.custom("Inter-Regular", size: 16))
-            check()
+            if emailTest() {
+                Image("Format OK")
+                    .resizable()
+                    .frame(width: 25, height: 20)
+            }
         }
         .padding(.horizontal, 16)
         .padding(.vertical, 14)
@@ -31,7 +25,7 @@ struct EmailTextfield: View {
         .overlay(
             RoundedRectangle(cornerRadius: 20)
                 .stroke(style: .init(lineWidth: 1))
-                .foregroundColor(formEmail() ? .primary2 : .hint)
+                .foregroundColor(emailTest() ? .primary2 : .hint)
         )
     }
 }
