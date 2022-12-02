97b7805363f97da1372240a9af0068fdd86548fc
hsj._.06 <gtw030488@gmail.com>
2022-12-02 17:36:13 +0900

    SignInViewModel

---
+++ DMSports/DMSports/Sources/Scene/Auth/SignIn/ViewModel/SignInViewModel.swift
@@ -0,0 +1,92 @@
+import Moya
+import Foundation
+import SwiftKeychainWrapper
+
+class SignInViewModel: ObservableObject {
+    let userClient = MoyaProvider<UsersService>(plugins: [MoyaLoggerPlugin()])
+    @Published var email: String = ""
+    @Published var password: String = ""
+    @Published var viewTag: Int?
+    @Published var showProgrees: Bool = false
+    @Published var showError: Bool = false
+    @Published var errorMessage: String = ""
+    func login() {
+        userClient.request(.login(email: email, password: password)) { res in
+            switch res {
+            case .success(let result):
+                switch result.statusCode {
+                case 200...201:
+                    DispatchQueue.main.async {
+                        let decoder = JSONDecoder()
+                        if let data = try? decoder.decode(TokenModel.self, from: result.data) {
+                            Token.accessToken = data.accessToken
+                            Token.refreshToken = data.refreshToken
+//                            self.getMyProFile()
+                            print("✅로그인 성공")
+                            print("🔊\(data.expiredAt)")
+                            KeychainWrapper.standard.set(true, forKey: "auto")
+                            KeychainWrapper.standard.set(self.email, forKey: "id")
+                            KeychainWrapper.standard.set(self.password, forKey: "pw")
+                            self.viewTag = 1
+                        } else {
+                            print("⚠️login docoder error")
+                        }
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        self.showProgrees = false
+                    }
+                case 400:
+                    DispatchQueue.main.async {
+                        self.errorMessage = "올바르지 않은 아이디/비밀번호 형식입니다."
+                        self.showError = true
+                        self.showProgrees = false
+                    }
+                case 401:
+                    DispatchQueue.main.async {
+                        self.errorMessage = "올바르지 않은 비밀번호 입니다."
+                        self.showError = true
+                        self.showProgrees = false
+                    }
+                case 404:
+                    DispatchQueue.main.async {
+                        self.errorMessage = "찾을 수 없는 아이디 입니다."
+                        self.showError = true
+                        self.showProgrees = false
+                    }
+                default:
+                    DispatchQueue.main.async {
+                        self.errorMessage = "알 수 없는 오류입니다. 문의 바랍니다!\n(code: \(result.statusCode))"
+                        self.showError = true
+                        self.showProgrees = false
+                    }
+                }
+            case .failure(let err):
+                print("⛔️login error: \(err.localizedDescription)")
+            }
+        }
+    }
+//    func getMyProFile() {
+//        userClient.request(.getMyprofile) { res in
+//            switch res {
+//            case .success(let result):
+//                switch result.statusCode {
+//                case 200:
+//                    if let data = try? JSONDecoder().decode(ProfileModel.self, from: result.data) {
+//                        Account.setAccount(
+//                            id: data.name,
+//                            email: data.email,
+//                            profileImagLink: data.profileImageUrl,
+//                            positions: data.positions
+//                        )
+//                    } else {
+//                        print("⚠️myProfile LG docoder error")
+//                    }
+//                default:
+//                    print(result.statusCode)
+//                }
+//            case .failure(let err):
+//                print("⛔️myProfile LG Error: \(err.localizedDescription)")
+//            }
+//        }
+//    }
+}
